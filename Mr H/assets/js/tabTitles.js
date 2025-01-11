c441bb7ed1f0d5a0126d01620cc792f0b7d33572
yuh <148779729+dedrh3vhgev3hrfh4r15@users.noreply.github.com>
2025-01-11 21:37:14 +0100

    Add files via upload

---
+++ Mr H/assets/js/tabTitles.js
@@ -0,0 +1,10 @@
+const tabTitles = ['@Mr H', '@reped', '@dead'];
+let index = 0x0;
+
+function updateTabTitle() {
+    document.title = tabTitles[index];
+    index = (index + 0x1) % tabTitles.length;
+}
+
+updateTabTitle();
+setInterval(updateTabTitle, 0x1f4); // Update tab title every 500ms
